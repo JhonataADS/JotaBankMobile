7c017239243bbc2035b2002c295daa05e510e743
jhonata <jhonata.ads@outlook.com>
2025-06-28 14:49:20 -0300

    cria estrutura de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Help } from './pages/help/help';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home},
+    {path: 'help', component: Help},
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
